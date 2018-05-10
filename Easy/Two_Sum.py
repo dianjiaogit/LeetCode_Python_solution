fee9701ff249edd1b8dc60e7debf224755bb1ed1
Jiao Dian <u6504904@uds.anu.edu.au>
2018-05-10 23:54:21 +1000

    push Two Sum

---
+++ Easy/Two_Sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range (len(nums)):
+            if nums[i] < target / 2:
+                try:
+                    if i < nums.index(target - nums[i]):
+                        return [i, nums.index(target - nums[i])]
+                    elif i > nums.index(target - nums[i]):
+                        return [nums.index(target - nums[i]), i]
+                except:
+                    pass
+        for i in range (len(nums) - 1):
+            if nums[i] == target / 2:
+                nums.remove(target / 2)
+                try:
+                    return [i, nums.index(target / 2) + 1]
+                except:
+                    pass
