c503d3b224dea293e2dfc25d27a80a5c4743940e
Jiao Dian <jiao.dianmail@gmail.com>
2018-07-21 20:43:57 +1000

    add remove duplicates from sorted list

---
+++ Easy/Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,34 @@
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+# Example 1:
+
+# Input: 1->1->2
+# Output: 1->2
+# Example 2:
+
+# Input: 1->1->2->3->3
+# Output: 1->2->3
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return head
+        if head.next == None:
+            return head
+        if head.val == head.next.val:
+            head.next = head.next.next
+            return Solution.deleteDuplicates(self, head)
+        else:
+            head.next = Solution.deleteDuplicates(self, head.next)
+            return head
