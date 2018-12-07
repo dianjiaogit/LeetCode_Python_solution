0d37bf0dd758a1cc482b35f58b3647b423932af5
Jiao Dian <jiao.dianmail@gmail.com>
2018-12-07 21:29:27 +1100

    add insert into a binary search tree

---
+++ Medium/Insert_into_a _Binary_Search_Tree.py
@@ -0,0 +1,54 @@
+# Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+# Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+# For example, 
+
+# Given the tree:
+#         4
+#        / \
+#       2   7
+#      / \
+#     1   3
+# And the value to insert: 5
+# You can return this binary search tree:
+
+#          4
+#        /   \
+#       2     7
+#      / \   /
+#     1   3 5
+# This tree is also valid:
+
+#          5
+#        /   \
+#       2     7
+#      / \   
+#     1   3
+#          \
+#           4
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root == None:
+            result = TreeNode(val)
+            return result
+        else:
+            if root.val > val:
+                root.left = Solution.insertIntoBST(self, root.left, val)
+            elif root.val < val:
+                root.right = Solution.insertIntoBST(self, root.right, val)
+            return root
