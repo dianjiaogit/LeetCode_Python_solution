8114a4af4e5e7292389e51a11aa09b2b7382567f
Jiao Dian <jiao.dianmail@gmail.com>
2019-12-05 19:08:52 +0800

    add house robber

--- Easy/House_Robber.py
+++ Easy/House_Robber.py
@@ -17,8 +17,12 @@
 
 
 class Solution:
-    def rob(self, nums) -> int:
+    def rob(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0
-        s = max(nums[0] + Solution.rob(self, nums[2:]), Solution.rob(self, nums[1:])) 
-        return s
+        if len(nums) < 3:
+            return max(nums)
+        nums[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
+        return nums[-1]
